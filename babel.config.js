4dc532daa8bcf062d7f36dd76532d647294c510a
insane <luojieyy@gmail.com>
2020-07-10 00:19:44 +0800

    feat: demo code

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        [
+            'import', {
+                "libraryName": "@doctorwork/one",
+                "style": false,
+            }, '@doctorwork/one'
+        ]
+    ]
+}
